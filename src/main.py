df0ca02e6884acabe117b75e81e6c894d6f0f5f6
erwan.vivien <erwan.vivien@epita.fr>
2021-02-07 15:25:04 +0100

    Bot: Added first commands and renamed from eGold -> eGLD

--- src/main.py
+++ src/main.py
@@ -12,14 +12,25 @@ DEV_IDS = [289145021922279425]
 
 ERRORS = []
 
-DISC_LNK = ""
+DISC_LNK = "https://discord.com/api/oauth2/authorize?client_id=807967570962939914&permissions=10304&scope=bot"
 
 token = utils.get_content("token_dev")
 
 
+CMDS = {
+    "egld$add": utils.add,
+    "egld$wallet": utils.add,
+    "egld$link": utils.add,
+
+    "egld$delete": utils.delete,
+    "egld$remove": utils.delete,
+    "egld$unlink": utils.delete,
+}
+
+
 class Client(discord.Client):
     async def on_ready(self):
-        print(f'[eGold] Logged on as {self.user}')
+        print(f'[eGLD] Logged on as {self.user}')
         print(f"invite link: ↓\n{DISC_LNK}")
         print('==============================================================================================')
         print()
@@ -28,7 +39,7 @@ class Client(discord.Client):
             await client.change_presence(
                 status=discord.Status.online,
                 activity=discord.Activity(
-                    name="eGold !",
+                    name="eGLD !",
                     type=discord.ActivityType.watching))
         except Exception as error:
             utils.log("on_ready", error,
