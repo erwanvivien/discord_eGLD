1b67df37e7a9218dddcc3e168fa4c63b9a15d75c
erwan.vivien <erwan.vivien@epita.fr>
2021-02-07 23:48:15 +0100

    Add: Can check for performances in last 24h

--- src/binance.py
+++ src/binance.py
@@ -9,4 +9,15 @@ def get_price(name):
     if not "price" in js:
         return -1
 
-    return float(js["price"])
+    return round(float(js["price"]), 2)
+
+
+def stats(name):
+    r = requests.get(
+        f"https://api.binance.com/api/v3/ticker/24hr?symbol={name}")
+    js = r.json()
+
+    if not "priceChange" in js:
+        return (-100000, -100000)
+
+    return (round(float(js["priceChange"]), 2), round(float(js["priceChangePercent"]), 2))
