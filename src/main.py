3e9515bd08073a63c68d15d3e3027e745fe44ded
erwan.vivien <erwan.vivien@epita.fr>
2021-02-08 12:26:15 +0100

    Prod mode

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ ERRORS = []
 
 DISC_LNK = "https://discord.com/api/oauth2/authorize?client_id=807967570962939914&permissions=10304&scope=bot"
 
-token = utils.get_content("token_dev")
+token = utils.get_content("token")
 
 last_log_file = datetime.datetime.now()
 
