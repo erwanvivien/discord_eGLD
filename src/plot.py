7f5437aecc3b48d7d87aa1bc9b1960d9a9c1ec58
erwan.vivien <erwan.vivien@epita.fr>
2021-02-09 11:29:01 +0100

    Added: Annotations to the graph when displaying it

--- src/plot.py
+++ src/plot.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import matplotlib.cbook as cbook
 
+import numpy as np
+
 import datetime
 import database as db
 
@@ -12,34 +14,36 @@ days = mdates.DayLocator()  # every month
 years_fmt = mdates.DateFormatter('%Y')
 
 
-def create_graph(period="day"):
-    sql = "SELECT * FROM prices ORDER BY date DESC LIMIT 500"
+def create_graph(period=1):
+    sql = "SELECT * FROM prices ORDER BY date DESC"
     rows = db.exec(sql)
 
     dates = [datetime.datetime.strptime(
         row[db.PRICES_DATE], "%Y-%m-%d %H:%M:%S.%f") for row in rows]
 
-    values = [row[db.PRICES_VAL] for row in rows]
+    dates = []
+    values = []
+    for row in rows:
+        row_date = datetime.datetime.strptime(
+            row[db.PRICES_DATE], "%Y-%m-%d %H:%M:%S.%f")
+        if datetime.datetime.now() - datetime.timedelta(days=period) <= row_date:
+            dates += [row_date]
+            values += [row[db.PRICES_VAL]]
 
     fig, ax = plt.subplots()
+
     ax.plot(dates, values)
-
-    ax.xaxis.set_major_locator(days)
-    ax.set_xlim(dates[-1], dates[0])
-
-    ymin = min(values)
-    xpos = values.index(ymin)
-    xmin = values[xpos]
 
     ymax = max(values)
     xpos = values.index(ymax)
-    xmax = values[xpos]
+    xmax = dates[xpos]
 
-    ax.set_ylim(xmin - 0.5, xmax + 0.5)
+    ymin = min(values)
+    xpos = values.index(ymin)
+    xmin = dates[xpos]
 
-    ax.annotate(f"max: {ymax}", xy=(xmax, ymax), xytext=(xmax, ymax+5))
-
-    fig.autofmt_xdate()
+    ax.annotate(f"max: {ymax}", xy=(xmax, ymax), xytext=(xmax, ymax + 0.1))
+    ax.annotate(f"max: {ymin}", xy=(xmin, ymin), xytext=(xmin, ymin - 0.1))
 
     plt.plot()
     plt.savefig(f'graph_{period}.png')
