aa24159611b5903fc252c1477611377d1e7a4d25
erwan.vivien <erwan.vivien@epita.fr>
2021-02-09 10:26:58 +0100

    First code to send graph for the last x days

---
+++ src/plot.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.cbook as cbook
+
+import datetime
+import database as db
+
+
+years = mdates.YearLocator()   # every year
+months = mdates.MonthLocator()  # every month
+days = mdates.DayLocator()  # every month
+years_fmt = mdates.DateFormatter('%Y')
+
+
+def create_graph(period="day"):
+    sql = "SELECT * FROM prices ORDER BY date DESC LIMIT 500"
+    rows = db.exec(sql)
+
+    dates = [datetime.datetime.strptime(
+        row[db.PRICES_DATE], "%Y-%m-%d %H:%M:%S.%f") for row in rows]
+
+    values = [row[db.PRICES_VAL] for row in rows]
+
+    fig, ax = plt.subplots()
+    ax.plot(dates, values)
+
+    ax.xaxis.set_major_locator(days)
+    ax.set_xlim(dates[-1], dates[0])
+
+    ymax = max(values)
+    xpos = values.index(ymax)
+    xmax = values[xpos]
+
+    ax.annotate(f"max: {ymax}", xy=(xmax, ymax), xytext=(xmax, ymax+5))
+
+    fig.autofmt_xdate()
+
+    plt.plot()
+    plt.savefig(f'graph_{period}.png')
